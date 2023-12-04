dcf798403f7d5d516dbc4ed7a7a43493e0aad6df
SafwenHali <93746781+SafwenHali@users.noreply.github.com>
2023-12-04 19:44:35 +0100

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -7,6 +7,6 @@ docker run --rm -d -p 5000:5000 --name node_app node:14 bash -c '
   echo "const express = require(\x27express\x27); const app = express(); app.get(\x27/\x27, (req, res) => { res.send(\x27Hello World!\x27); }); app.listen(3000, () => { console.log(\x27Server is running on port 3000\x27); });" > index.js &&
   node index.js
 '
-sleep 15
+sleep 10
 
 curl http://localhost:5000
